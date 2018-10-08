3e977f5d97a420707740d13d824cbb4755d44ce8
XSplitGaming <37336451+XSplitGaming@users.noreply.github.com>
2018-10-08 20:39:41 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -161,6 +161,26 @@ if(message.content.startsWith(prefix + "uplay")){
     });
 }
 	
+module.exports.run = async (bot, message, args) => {
+
+  let serverembed = new Discord.RichEmbed()
+  .setTitle("Server Information")
+  .setColor("0ED4DA")
+  .setThumbnail(message.guild.iconURL)
+  .addField('Name', `${message.guild.name} (${message.guild.nameAcronym})`, true)
+  .addField('Server Owner', message.guild.owner.user.tag, true)
+  .addField("Server Create Date", message.guild.createdAt, true)
+  .addField("Member Count", message.guild.memberCount, true)
+
+  return message.channel.send(serverembed);
+}
+
+
+module.exports.help = {
+  name: "serverinfo",
+  description: "Gathers information about the server."
+}
+	
 });
 
 client.login(process.env.TOKEN);
