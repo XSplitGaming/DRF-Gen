7617de170ad8f821d442e40db1bbaa4e9e38e7ac
XSplitGaming <37336451+XSplitGaming@users.noreply.github.com>
2018-10-08 12:11:28 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -69,7 +69,7 @@ client.on("message", (message) => {
     .addField("help", "This command.")
     .addField("minecraft", "Get a Minecraft account.")
     .addField("spotify", "Get a Spotify account.")
-    .addField("hulu", "Get a Hulu account.")
+    .addField("uplay", "Soon.")
     .addField("uptime", "Show the bot's uptime.")
 
     message.author.send(embed);
